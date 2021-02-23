b1ef1d103087f0d75c494428857b5e26e9990b01
Ivan Reyes <ivanwilf@hotmail.com>
2021-02-23 17:31:18 +0900

    added the rout for bookings

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :clinics
-   # TO DO nest the reviews and the bookings
+  resources :clinics do
+    resources :bookings, only: [:new, :create]
+    # TO DO nest the reviews
+  end
+   resources :bookings, only: [:show, :edit, :update, :destroy]
 end
