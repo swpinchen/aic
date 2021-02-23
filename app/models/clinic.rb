de4bd6e3f8b61e3b6cb5f6cceaccf5ec947d5dbd
Ivan Reyes <ivanwilf@hotmail.com>
2021-02-23 11:23:01 +0900

    models validations and associations

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -1,3 +1,9 @@
 class Clinic < ApplicationRecord
   belongs_to :user
+  has_many :reviews
+  has_many :bookings
+
+  validates :name, presence: true
+  validates :location, presence: true
+  validates :description, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :clinic
+
+  validates :rating, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10}
+  validates :content, presence: true
 end
