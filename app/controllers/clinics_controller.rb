bd6738b9482eae8b87e4dee8ad7790a86a470182
Ivan Reyes <ivanwilf@hotmail.com>
2021-02-23 16:53:11 +0900

    added Pundit into the app

--- app/controllers/clinics_controller.rb
+++ app/controllers/clinics_controller.rb
@@ -1,20 +1,23 @@
 class ClinicsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   def index
-    @clinics = Clinic.all
+    @clinics = policy_scope(Clinic)
   end
 
   def show
     @clinic = Clinic.find(params[:id])
+    authorize @clinic
   end
 
   def new
     @clinic = Clinic.new
+    authorize @clinic
   end
 
   def create
     @clinic = Clinic.create(clinic_parms)
     @clinic.user = current_user
+    authorize @clinic
     if @clinic.save
       redirect_to clinics_path
     else
---
+++ app/policies/clinic_policy.rb
@@ -0,0 +1,19 @@
+class ClinicPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def show?
+    true
+  end
+end
