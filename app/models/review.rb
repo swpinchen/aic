097997e38c5136449cfbd0584d012ec392bef67b
Ivan Reyes <ivanwilf@hotmail.com>
2021-02-23 11:41:22 +0900

    fixed the ration range 1 - 5

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :clinic
 
-  validates :rating, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10}
+  validates :rating, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
   validates :content, presence: true
 end
