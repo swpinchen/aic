76b90681f57ae630f5eb364ce58d450d57fa34b4
Ivan Reyes <ivanwilf@hotmail.com>
2021-02-23 14:55:10 +0900

    added the picture feature to the cliics model and updted the views

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -2,6 +2,7 @@ class Clinic < ApplicationRecord
   belongs_to :user
   has_many :reviews
   has_many :bookings
+  has_one_attached :photo
 
   validates :name, presence: true
   validates :location, presence: true
